dfee39772e77c839c600c79ddb25aa6c2a92daad
donggyeong <donggyeong@localhost.localdomain>
2022-05-17 05:15:24 +0900

    일일 업무 기능 보완

--- nicework/bsnlog/views/hist_views.py
+++ nicework/bsnlog/views/hist_views.py
@@ -6,6 +6,7 @@ from ..forms import BslHistoryForm
 from ..models import BslHistory
 from django.utils import timezone
 from django.core.paginator import Paginator
+from django.db.models import Q
 
 
 @login_required(login_url='common:login')
@@ -16,21 +17,33 @@ def history(request):
 
     # 페이지 당 10개씩 보여주기
     page = request.GET.get('page', '1')
+    kw = request.GET.get('kw', '')  # 검색어
+    if kw:
+        mylist = mylist.filter(
+            Q(created_at__icontains=kw) |
+            Q(contents__icontains=kw)
+        ).distinct()
     paginator = Paginator(mylist, 10)    
     page_obj = paginator.get_page(page)
 
-    context = {'mylist': page_obj}
+    context = {'myuser': myuser, 'mylist': page_obj, 'page': page, 'kw': kw}
     return render(request, 'bsnlog/bsnlog_hist.html', context)
 
 
 @login_required(login_url='common:login')
 def update(request, bsnlog_id):
+    myuser = get_object_or_404(MyUser, email=request.user.email)
+    
     businesslog = get_object_or_404(BslHistory, pk=bsnlog_id)
 
-    # 일지 작성자가 로그인한 계정과 다른지 확인
-    if request.user != businesslog.employee:
-        messages.error(request, '수정권한이 없습니다')
-        return redirect('bsnlog:hist')
+    # 관리자 또는 매니저인 경우
+    if myuser.is_mgr | myuser.is_admin:
+        pass
+    else:
+        # 일지 작성자가 로그인한 계정과 다른지 확인
+        if request.user != businesslog.employee:
+            messages.error(request, '수정권한이 없습니다')
+            return redirect('bsnlog:hist')
 
     if request.method == "POST": # 양식 작성하여 POST
         form = BslHistoryForm(request.POST)
@@ -38,7 +51,11 @@ def update(request, bsnlog_id):
             businesslog.contents = form.cleaned_data.get('contents')
             businesslog.updated_at = timezone.now()
             businesslog.save()
-            return redirect('bsnlog:hist')
+
+            if myuser.is_mgr | myuser.is_admin:
+                return redirect('bsnlog:toth')
+            else:
+                return redirect('bsnlog:hist')
     else: # GET 페이지 요청
         form = BslHistoryForm()
 
@@ -48,24 +65,38 @@ def update(request, bsnlog_id):
 
 @login_required(login_url='common:login')
 def delete(request, bsnlog_id):
+    myuser = get_object_or_404(MyUser, email=request.user.email)
+
     businesslog = get_object_or_404(BslHistory, pk=bsnlog_id)
-    if request.user != businesslog.employee:
-        messages.error(request, '삭제권한이 없습니다')
-        return redirect('bsnlog:updt', bsnlog_id=businesslog.id)
+    # 관리자 또는 매니저인 경우
+    if myuser.is_mgr | myuser.is_admin:
+        pass
+    else:
+        if request.user != businesslog.employee:
+            messages.error(request, '삭제권한이 없습니다')
+            return redirect('bsnlog:updt', bsnlog_id=businesslog.id)
     businesslog.delete()
     return redirect('bsnlog:hist')
 
 
 @login_required(login_url='common:login')
 def totalhistory(request):
-    # 로그인 계정으로 등록한 일지 리스트 가져오기
     myuser = get_object_or_404(MyUser, email=request.user.email)
-    mylist = BslHistory.objects.filter(employee=myuser).order_by('-created_at')
+
+    # 전체 일지 리스트 가져오기
+    mylist = BslHistory.objects.filter().order_by('-created_at')
 
     # 페이지 당 10개씩 보여주기
     page = request.GET.get('page', '1')
+    kw = request.GET.get('kw', '')  # 검색어
+    if kw:
+        mylist = mylist.filter(
+            Q(employee__realname__icontains=kw) |
+            Q(created_at__icontains=kw) |
+            Q(contents__icontains=kw)
+        ).distinct()
     paginator = Paginator(mylist, 10)    
     page_obj = paginator.get_page(page)
 
-    context = {'mylist': page_obj}
-    return render(request, 'bsnlog/bsnlog_hist.html', context)
+    context = {'myuser': myuser, 'mylist': page_obj, 'page': page, 'kw': kw}
+    return render(request, 'bsnlog/bsnlog_toth.html', context)
